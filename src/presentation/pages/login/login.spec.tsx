fe50d9502a1abdc571272d35bc2bf8b8f498dbf0
gabrielinaciosouza <gabriel.sert9@gmail.com>
2021-06-10 18:22:53 -0300

    test: ensure login start with inputs with required state

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import { render } from '@testing-library/react'
 import Login from './login'
+import '@testing-library/jest-dom/extend-expect'
 
 describe('Login Component', () => {
   test('Should start with initial state', () => {
-    const { getByTestId } = render(<Login />)
+    const { getByTestId, container } = render(<Login />)
     const errorWrap = getByTestId('error-wrap')
     expect(errorWrap.childElementCount).toBe(0)
     const submitButton = getByTestId('submit') as HTMLButtonElement
     expect(submitButton.disabled).toBe(true)
+    const emailStatus = getByTestId('email-status')
+    expect(emailStatus.title).toBe('')
+    expect(container.firstChild).toHaveStyle('border: 1px solid $white;')
+    const passwordStatus = getByTestId('password-status')
+    expect(passwordStatus.title).toBe('')
+    expect(container.firstChild).toHaveStyle('border: 1px solid $white;')
   })
 })
